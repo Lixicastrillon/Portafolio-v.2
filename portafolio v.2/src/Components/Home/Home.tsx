f1b653cb6350fefe0c6ea85fbcf8bc585d7c9ce1
lixicastrillon <xcl_xioma@hotmail.com>
2024-02-23 12:20:30 -0500

    home

--- portafolio v.2/src/Components/Home/Home.tsx
+++ portafolio v.2/src/Components/Home/Home.tsx
@@ -2,33 +2,21 @@ import "./Home.scss";
 
 const Home = () => {
   return (
-    <div className="full-size">
-      <div className="container">
-        <div id="section-home" className="section-home">
-          <div className="text-zone">
-            <h1>Hola!</h1>
-            <br />
-            <h1>Bienvenido a mi página web, soy Liced.</h1>
-            <br />
-            <h1>Web developer</h1>
-            <p className="gray-text">Full stack Developer</p>
-          </div>
-          <a href="#" className="contact-button">
-            <div>
-              <span className="bg"></span>
-              <span className="base"></span>
-              <span className="text">Contact me</span>
-            </div>
-          </a>
-          <div className="scroll-down">
-            <span>scroll down</span>
-            <i className="bi bi-arrow-down"></i>
-          </div>
-          <div className="scroll-down scroll-down-left">
-            <span>scroll down</span>
-            <i className="bi bi-arrow-down"></i>
-          </div>
+    <div className="container full-size Home">
+      <div className="section-home h-100">
+        <div className="text-zone">
+          <h1>Hola!</h1>
+          <h1>Bienvenidos, soy Liced</h1>
+          <h1>Web developer</h1>
+          <p>Web Full stack Developer</p>
         </div>
+        <a href="#" className="contact-button w-100">
+          <div>
+            <span className="bg"></span>
+            <span className="base"></span>
+            <span className="text">Contact me</span>
+          </div>
+        </a>
       </div>
     </div>
   );
