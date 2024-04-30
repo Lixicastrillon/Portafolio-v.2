f84aaf9dbfdb139dd695e5eac0688e15185628ec
lixicastrillon <xcl_xioma@hotmail.com>
2024-04-29 21:51:12 -0500

    responsive

--- portafolio v.2/src/Components/Projects/Projects.tsx
+++ portafolio v.2/src/Components/Projects/Projects.tsx
@@ -8,22 +8,20 @@ interface projectProps {
 
 const Projects = ({ cardp }: projectProps) => {
   return (
-    <div className="container d-flex">
-      <div className="projects d-flex flex-column z-3">
-        <div className="d-flex flex-column align-items-center">
-          <div className="contornoH1">
-            <h1>Trabajos</h1>
-          </div>
-          <div className="work">Works</div>
-          {/* no se traduce será parte del fondo*/}
-          <p>
-            He tenido el privilegio de formar parte de equipos que contribuyeron
-            significativamente a mi desarrollo profesional en el ámbito del
-            desarrollo web. Durante estas experiencias, mi enfoque estuvo
-            orientado hacia la garantía de la funcionalidad y el rendimiento
-            óptimo de las aplicaciones.
-          </p>
+    <div className="container full-size d-flex ">
+      <div className="projects d-flex flex-column align-items-center justify-content-evenly z-3">
+        <div className="contornoH1">
+          <h1>Trabajos</h1>
         </div>
+        <div className="work">Works</div>
+        {/* no se traduce será parte del fondo*/}
+        <p className="box-description-projets">
+          He tenido el privilegio de formar parte de equipos que contribuyeron
+          significativamente a mi desarrollo profesional en el ámbito del
+          desarrollo web. Durante estas experiencias, mi enfoque estuvo
+          orientado hacia la garantía de la funcionalidad y el rendimiento
+          óptimo de las aplicaciones.
+        </p>
         <div className="cardsProjects  row">
           {cardp.map((card) => (
             <ProjectCard key={card.id} card={card} />
